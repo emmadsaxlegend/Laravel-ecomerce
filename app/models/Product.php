e1d74917057e5d9d46d1b2eab79a2ab4c3f7a96d
Emmanuel <eokereke47@gmail.com>
2020-07-30 21:19:33 +0100

    First Commit

---
+++ app/models/Product.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable=[
+
+    'product_name',
+    'price',
+    'slug',
+    'status',
+    'category_id'
+    ];
+
+    public function category(){
+        return $this->belongsTo('App\models\ProductCategory');
+
+    }
+    public function product_image(){
+        return $this->hasMany('App\models\ProductImage');
+    }
+}
